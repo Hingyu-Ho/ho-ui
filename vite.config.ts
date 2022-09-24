6020b654e33fa653a1b01ee58e6f15b9444b7995
Timo <timoho@163.com>
2022-09-24 15:56:06 +0800

    build(modules): 配置 vite.config.js 构建库模式

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,47 @@
-import legacy from "@vitejs/plugin-legacy";
-import react from "@vitejs/plugin-react";
-import { fileURLToPath } from "url";
-import { defineConfig } from "vite";
+import legacy from '@vitejs/plugin-legacy'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
+
+const isDevelopment = process.env.NODE_ENV === 'development'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [legacy(), react()],
+  plugins: isDevelopment ? [(legacy(), react())] : [],
   resolve: {
     alias: {
       // for TypeScript path alias import like : @/x/y/z
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:8080",
+      '/api': {
+        target: 'http://localhost:8080',
         secure: false,
-        rewrite: path => path.replace(/^\/api/, ""),
-      },
-    },
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
-});
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/components/index.tsx'),
+      // 暴露的全局变量
+      name: 'EfUI',
+      // 输出的包文件名
+      fileName: (format) => `ef-ui.${format}.js`
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['react', 'react-dom'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
+})
